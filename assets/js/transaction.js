f1decc77f114c9b048b19e0c11a904354caea9c1
Bumblebee124 <M.Asmirkhan05@gmail.com>
2022-03-08 19:20:54 -0600

    created new files, removed inline css&js. Fixed some information

---
+++ assets/js/transaction.js
@@ -0,0 +1,7 @@
+function close_transaction() {
+    alert("Error, the developers haven't implemented this feature yet. Press Ok to redirect.");
+    location.replace("../../index.html");
+}
+function redirect() {
+    location.replace("../../index.html");
+}
